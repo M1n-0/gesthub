17c91e5fa491aa458ec8c83785d17d1d26a1b081
M1n-0 <nino.labat@ynov.com>
2025-04-14 10:48:11 +0200

    add base web + change a container

--- web/app.py
+++ web/app.py
@@ -1,6 +1,7 @@
 # flask-app/app.py
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask import render_template
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://flaskuser:flaskpass@mariadb/flaskdb'
@@ -8,9 +9,9 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-@app.route('/')
-def index():
-    return 'Bienvenue sur l’intranet !'
-
+    @app.route('/')
+    def index():
+        return render_template('index.html')
+        
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
